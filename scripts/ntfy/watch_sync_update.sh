9698e08688b520006ab8fc6d28d0fe092907e4f0
Henry James <hj_320+github@protonmail.com>
2025-08-02 21:21:31 +0100

    Clean  up the sync watcher

--- scripts/ntfy/watch_sync_update.sh
+++ scripts/ntfy/watch_sync_update.sh
@@ -2,43 +2,35 @@
 
 WATCH_DIR="/home/casa/locations/sync"
 NTFY_TOPIC="whale_server_1"
-BUFFER_TIME=15  # Seconds to batch events
+BUFFER_TIME=600  # Seconds to batch events
 
 event_file=$(mktemp)
-
 trap "rm -f $event_file" EXIT
 
 # Background process to summarize and send notifications
 (
     while true; do
         if [[ -s $event_file ]]; then
-            # Extract and count CREATE and DELETE events
             read create_count delete_count <<< $(sort "$event_file" | uniq | \
-                awk -F'[()]' '{ print $2 }' | tr ',' '\n' | \
+                awk -F'|' '{ print $2 }' | \
                 awk '
                     BEGIN { create=0; del=0 }
-                    $1 == "CREATE" { create += 1 }
-                    $1 == "DELETE" { del += 1 }
+                    $1 ~ /CREATE/ { create += 1 }
+                    $1 ~ /DELETE/ || $1 ~ /MOVED_FROM/ { del += 1 }
                     END { print create, del }')
-
 
             summary=""
 
             if (( create_count > 0 )); then
-                if (( create_count == 1 )); then
-                    summary+="1 new file"
-                else
-                    summary+="$create_count new files"
-                fi
+                summary+="$create_count new file"
+                [[ $create_count -gt 1 ]] && summary+="s"
             fi
 
             if (( delete_count > 0 )); then
-                if [[ -n "$summary" ]]; then summary+=" and "; fi
-                if (( delete_count == 1 )); then
-                    summary+="1 file deleted"
-                else
-                    summary+="$delete_count files deleted"
-                fi
+                [[ -n "$summary" ]] && summary+=" and "
+                summary+="$delete_count file"
+                [[ $delete_count -gt 1 ]] && summary+="s"
+                summary+=" deleted"
             fi
 
             if [[ -n "$summary" ]]; then
@@ -53,7 +45,8 @@ trap "rm -f $event_file" EXIT
 ) &
 
 # Watch for file events
-inotifywait -m -r -e create -e delete --format '%w%f (%e)' "$WATCH_DIR" |
-while read FILE_EVENT; do
-    echo "$FILE_EVENT" >> "$event_file"
+inotifywait -m -r \
+    -e create -e delete -e move --format '%w%f|%e' "$WATCH_DIR" |
+while IFS='|' read -r FILE EVENT; do
+    echo "$FILE|$EVENT" >> "$event_file"
 done
