ba364cbbb8792e56bc7392e39a3541f2f8c68125
Henry James <hj_320@protonmail.com>
2026-01-03 23:16:37 +0000

    Move file

--- scripts/virusEvent.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Updates the .env file in the event that a virus has been found.
-
-env_file_location="/home/henry/Software/clamav_homepage/updates/.env"
-echo "virusEvent=True" > $env_file_location
