241e08630d8cfd68ed0b2e15d70f2cfd19949053
Henry James <hj_320+github@protonmail.com>
2026-01-31 22:24:07 +0000

    Set up DNF check for my fedora systems

--- scripts/check_dnf_updates.sh
+++ scripts/check_dnf_updates.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-dnf check-update
+DEVICE_NAME="Be Quiet PC"
+DEVICE_EMOJI="⬆️"
 
-# Get the list of security updates with Important and Critical severity.
-updates=$(dnf updateinfo list --sec-severity=Important --sec-severity=Critical | tail -n +2)        
-
-# Count the number of lines in the output.
-count=$(echo "$updates" | wc -l)
-
-# Check if there are any updates.
-if [ "$count" -gt 1 ]; then
-
-    webhook="https://discord.com/api/webhooks/1279539607922409532/jUJmzeZNC41Cdz6twOf2jMWb-4SDYgIz7LdAanfb8iQVHrhnH4hxtxrq8lOzjbX1Y23L"
-
-    message="There are Important or Critical security updates available:\n$updates"
-    formatted_message=$(printf "There are important or moderate security updates available:\n\n%s" "$updates")
-
-    # Send a message to the Discord webhook.
-    python /home/henry/Software/sftp_checkup_homepage/app/send_to_discord.py "DNF Update Checker" "$formatted_message" "$webhook"
+if [ "$(date +%u)" -ne 7 ]; then
+    echo "Not Sunday, exiting early."
+    exit 0
 fi
+
+UPDATES=$(dnf check-update --quiet | grep -E '^[a-zA-Z0-9]' | wc -l)
+SECURITY_UPDATES=$(dnf updateinfo list --security --quiet | grep -E '^[A-Z]' | wc -l)
+DOWNLOAD_SIZE=$(dnf upgrade --assumeno 2>&1 | sed -nE 's/.*Need to download ([0-9.]+ [A-Za-z]+).*/\1/p')
+
+curl -d "${DEVICE_EMOJI} ${DEVICE_NAME} - $UPDATES updates available totalling $DOWNLOAD_SIZE. $SECURITY_UPDATES are security updates." "ntfy.sh/whale_server_1"
